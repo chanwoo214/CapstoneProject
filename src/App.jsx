4313634d4b4460965dd686cdbfc40b3b9e43ed3f
chanwoo214 <chanwooshim@gmail.com>
2023-09-19 00:07:48 +1200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/1.login/Login';
+import ProductAll from './pages/2.productAll/ProductAll';
+import Detail from './pages/3.detail/Detail';
+import Navbar from './components/header/Navbar'; 
+
+//1. Product page, Login page, Product Details Page
+//1.1 Create navbar
+//2. In the product page, user can view all the products
+//3. When LOGIN Button is clicked, login page appears.
+//4. When Product detail page is clicked, if not logged in, login page appears
+//5. When logged in, user can view the detailed page
+//6. When log out button is clicked, user logs out and back to the login page and cannot view the detailed page
+//7. When user is logged in, login icon changes.
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+    <Router>
+      <Routes>
+      
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/' element={<ProductAll />} />
+          <Route exact path='/product/:id' element={<Detail />} />
+      
+      </Routes>
+      </Router>
+      </div>
+
+  )
+}
+
+export default App
---
+++ src/components/header/Navbar.jsx
@@ -0,0 +1,35 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+
+
+const Navbar = () => {
+  const menuList = ["Home", "Mortgage", "About Us", "Contact Us"]
+  return (
+    <div>
+      <div>
+        <div className="login-button">
+          <FontAwesomeIcon icon={faUser} />
+          <div>Login</div>
+        </div>
+      </div>
+      <div className="nav-section">
+        < img width={100} src="https://logo.com/image-cdn/images/kts928pd/production/167fb8f48a301faab6df0e1b1cdf2fa2b1833355-410x412.png?w=1080&q=72" />
+      </div>
+      <div className="menu-area">
+          <ul className = "menu-list">
+            {menuList.map((menu) => (
+              <li>{menu}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+
+        </div>
+      </div>
+  
+
+
+  )
+}
+
+export default Navbar
