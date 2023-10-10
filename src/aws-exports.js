1b5645c8ddc576f7e58a7f8614c7934d950adffd
Chan Woo Shim <chanwooshim@gmail.com>
2023-10-10 19:40:43 +1300

    deploy

---
+++ src/aws-exports.js
@@ -0,0 +1,28 @@
+const awsexport = {
+    "aws_project_region": "ap-southeast-2",
+    "aws_cognito_region": "ap-southeast-2",
+    "aws_user_pools_id": "ap-southeast-2_k9LWJimnD",
+    "aws_user_pools_web_client_id": "716i9co4oh26oslc4vnqvvdsoh",
+    "oauth": {},
+    "aws_cognito_username_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": [
+            "REQUIRES_LOWERCASE",
+            "REQUIRES_UPPERCASE",
+            "REQUIRES_NUMBERS",
+            "REQUIRES_SYMBOLS"
+        ]
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
+};
+
+export default awsexport;
